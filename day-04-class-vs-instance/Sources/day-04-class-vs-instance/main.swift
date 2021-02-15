45e4664812b103db6141fa5727535d85ae59e067
Jon Gentry <jonmarkgentry@gmail.com>
2021-02-15 10:17:45 -0800

    add solution

--- day-04-class-vs-instance/Sources/day-04-class-vs-instance/main.swift
+++ day-04-class-vs-instance/Sources/day-04-class-vs-instance/main.swift
@@ -1,1 +1,29 @@
-print("Hello, world!")
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        // Add some more code to run some checks on initialAge
+        if initialAge >= 0 {
+          self.age = initialAge
+        } else {
+          self.age = 0
+          print("Age is not valid, setting age to 0.")
+        }
+    }
+
+    func amIOld() {
+        // Do some computations in here and print out the correct statement to the console
+        if age < 13 {
+          print("You are young.")
+        } else if age >= 13 && age < 18 {
+          print("You are a teenager.")
+        } else {
+          print("You are old.")
+        }
+    }
+
+    func yearPasses() {
+        // Increment the age of the person in here
+        self.age += 1
+    }
+}
