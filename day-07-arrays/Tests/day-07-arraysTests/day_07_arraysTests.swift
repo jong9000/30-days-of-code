16108de046fed66deb66014693f6ee511df15ba1
Jon Gentry <jonmarkgentry@gmail.com>
2021-02-27 17:15:47 -0800

    Refactor test suite to accept bundled test data files

--- day-07-arrays/Tests/day-07-arraysTests/day_07_arraysTests.swift
+++ day-07-arrays/Tests/day-07-arraysTests/day_07_arraysTests.swift
@@ -2,31 +2,55 @@ import XCTest
 import class Foundation.Bundle
 
 final class day_07_arraysTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    func testCases() throws {
 
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
 
-        let fooBinary = productsDirectory.appendingPathComponent("day-07-arrays")
+        guard let testDataInputArray = Bundle.module.urls(forResourcesWithExtension: "txt", subdirectory: "TestsData/Input") else {
+          print("⛔️ Unable to access test case data.")
+          return
+        }
+      
+        guard let testDataOutputArray = Bundle.module.urls(forResourcesWithExtension: "txt", subdirectory: "TestsData/Output") else {
+          print("⛔️ Unable to access test case data.")
+          return
+        }
+      
+        for n in 0..<testDataInputArray.count {
+          
+          print("⚠️ Testing \(testDataInputArray[n].lastPathComponent)")
+          
+          let stdin = FileHandle(forReadingAtPath: testDataInputArray[n].path)
+          
+          let fooBinary = productsDirectory.appendingPathComponent("day-07-arrays")
 
-        let process = Process()
-        process.executableURL = fooBinary
+          let process = Process()
+          process.executableURL = fooBinary
 
-        let pipe = Pipe()
-        process.standardOutput = pipe
+          let pipe = Pipe()
+          process.standardInput = stdin
+          process.standardOutput = pipe
+          
+          try? process.run()
+          process.waitUntilExit()
 
-        try process.run()
-        process.waitUntilExit()
+          let data = pipe.fileHandleForReading.readDataToEndOfFile()
+          let output = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines)
+          
+          let matchingOutputFileIndex = testDataOutputArray.firstIndex { $0.lastPathComponent == testDataInputArray[n].lastPathComponent }
+          
+          let expectedOutput = try? String(contentsOf: testDataOutputArray[matchingOutputFileIndex!]).trimmingCharacters(in: .whitespacesAndNewlines)
 
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
+          XCTAssertEqual(output, expectedOutput)
+          
+        }
+      
 
-        XCTAssertEqual(output, "Hello, world!\n")
+
+
     }
 
     /// Returns path to the built products directory.
@@ -42,6 +66,6 @@ final class day_07_arraysTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testCases", testCases),
     ]
 }
